0cc1918c4a6a4b44a39a2ab3ec98dfed50eacafe
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2022-08-22 23:20:34 +0530

    add transition component for page

---
+++ src/lib/components/TransitionPage.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { fly, blur, slide, fade } from 'svelte/transition';
+    import { cubicOut } from "svelte/easing";
+
+	export let url: URL;
+</script>
+
+{#key url}
+<div class="flex-auto overflow-y-auto"
+in:fly={{ x:-100, duration: 300, easing:cubicOut }}
+>
+<slot />
+</div>
+{/key}
