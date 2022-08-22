da539a80ce98eefb6cc8d51520b9df2b481aba23
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2022-08-22 22:50:30 +0530

    add active class to active sidebar item

--- src/lib/components/SidebarItem.svelte
+++ src/lib/components/SidebarItem.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
 	import type SidebarItemType from '../types/SidebarItemType';
+	import { page } from "$app/stores";
 
 	export let item: SidebarItemType;
 </script>
 
+<style>
+	.active {
+		@apply text-emerald-200 bg-emerald-900 shadow-lg
+	}
+</style>
+
 <a href={item.loc}>
 	<div
-		class="my-4 hover:bg-emerald-900 text-emerald-900 hover:text-emerald-200  p-2 rounded-full hover:shadow-xl shadow-none transition-all duration-200"
+		class="my-4 hover:bg-emerald-700 text-emerald-900 hover:text-emerald-100  p-2 rounded-full hover:shadow-xl shadow-none transition-all duration-200 {$page.url.pathname === item.loc ? 'active' : ''}"
 	>
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
