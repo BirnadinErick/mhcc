61dcf860f3f4b55b96c1ebe2e00d31c6103f993c
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2022-10-02 04:49:41 +0530

    move sql folder to root

--- src-tauri/migrations/2022-08-10-112926_create_staffs/up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE staffs
-(
-    staff_id      INTEGER PRIMARY KEY,
-    name          TEXT    NOT NULL,
-    uname         TEXT    NOT NULL UNIQUE,
-    passwd        TEXT    NOT NULL,
-    role          INTEGER NOT NULL DEFAULT 1,
-    date_enrolled INTEGER NOT NULL
-);
