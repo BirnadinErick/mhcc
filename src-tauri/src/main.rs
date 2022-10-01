a37de6d7ec3913feeb06be0799e142844bb8c7b2
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2022-10-02 04:51:54 +0530

    bend tweaks-check

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,14 +3,31 @@
     windows_subsystem = "windows"
 )]
 
+// use directories::ProjectDirs;
+use surrealdb::{Datastore, Session};
+
+#[tauri::command]
+async fn test_db() -> Result<(), String> {
+    // get local datadir
+
+    let db = Datastore::new("http://localhost:2007").await?;
+    let ses = Session::for_kv();
+    let ast = "USE NS mhcc DB main; SELECT * FROM staff;";
+    let res = db.execute(ast, &ses, None, false).await?;
+    println!("{:?}", res);
+
+    Ok(())
+}
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hey there, {} how are you?", name)
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, test_db])
         .run(tauri::generate_context!())
         .expect("couldn't start MHCC!");
 }
