571363291d600a465a3dd426af30a117ba6d6c05
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2023-02-06 20:42:50 +0530

    feat!: remove state.rs

--- src-tauri/src/stocks/state.rs
+++
@@ -1,8 +0,0 @@
-use sqlx::PgPool as Pool;
-use super::models::{GetStock, AddStock};
-
-pub struct StocksState {}
-
-impl StocksState {
-    pub async fn insert(new_stock: AddStock, pool: &Pool) {}
-}
