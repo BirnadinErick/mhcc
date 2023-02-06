24523aa225f309228fd1ed58b6c00bf6f74faffc
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2023-02-06 20:42:34 +0530

    feat!: remove delete_stock from PgAdapter

--- src-tauri/src/ports.rs
+++ src-tauri/src/ports.rs
@@ -7,6 +7,6 @@ pub trait StockService: {
     async fn add_stock(&self, add_stock: &AddStock) -> u64;
     async fn get_stock(&self, offset: f64) -> Vec<GetStock>;
     async fn update_stock(&self, updated_stock: GetStock) -> u64;
-    async fn delete_stock(&self, id: i64) -> bool;
+    // async fn delete_stock(&self, id: i64) -> bool;
     async fn search_stock(&self, query: String) -> Vec<GetStock>;
 }
