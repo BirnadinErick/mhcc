e3b66a933d2851e0a1b1bed31ae3df391c0b7618
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2023-02-08 20:28:31 +0530

    feat!: rename visiotrs to patients

---
+++ src/routes/patients/+page.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<title>Patients | MHCC</title>
+</svelte:head>
+
+<div class="flex justify-between my-4">
+	<h2 class="py-4 text-2xl font-serif">Patients.</h2>
+</div>
--- src/routes/visitors/+page.svelte
+++
@@ -1,7 +0,0 @@
-<svelte:head>
-	<title>Visitors | MHCC</title>
-</svelte:head>
-
-<div class="flex justify-between my-4">
-	<h2 class="py-4 text-2xl font-serif">Visitors.</h2>
-</div>
